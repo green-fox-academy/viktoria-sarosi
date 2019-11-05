15d3f3de7e545365324abe83610643c61e9b1683
viktoria-sarosi <domanchana@gmail.com>
2019-11-05 17:05:45 +0100

    rainbow box random and really rainbow

--- week-06/Drawings/src/ReallyRainbowBoxFunction.java
+++ week-06/Drawings/src/ReallyRainbowBoxFunction.java
@@ -4,22 +4,40 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class RainbowBoxFunction{
+public class ReallyRainbowBoxFunction{
     public static void mainDraw(Graphics graphics) {
         // Create a square drawing function that takes 3 parameters:
         // The square size, the fill color, graphics
         // and draws a square of that size and color to the center of the canvas.
         // Create a loop that fills the canvas with rainbow colored squares (red, orange, yellow, green, blue, indigo, violet).
-       // rainbowBoxes;
-        for (int i = 0; i < 7; i++) {
-            int a = 320 - i*60;
-            rainbowBoxes(a,graphics);
-
+        // rainbowBoxes;
+        for (int i = 0; i < 6; i++) {
+            int a = 320 - i * 60;
+            Color color = Color.yellow;
+            if (i == 0) {
+                color = Color.red;
+            }
+            else if (i == 1) {
+                color = Color.orange;
+            }
+            else if (i == 2) {
+                color = Color.yellow;
+            }
+            else if (i == 3) {
+                color = Color.green;
+            }
+            else if (i == 4) {
+                color = Color.blue;
+            }
+            else if (i == 5) {
+                color = Color.magenta;
+            }
+            rainbowBoxes(a, color, graphics);
         }
     }
-    private static void rainbowBoxes(int size, Graphics graphics) {
-            graphics.setColor(new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256)));
-            graphics.fillRect(WIDTH/2-size/2,HEIGHT/2-size/2, size,size);
+    private static void rainbowBoxes(int size, Color color, Graphics graphics) {
+        graphics.setColor(color);
+        graphics.fillRect(WIDTH/2-size/2,HEIGHT/2-size/2, size,size);
     }
 
 
@@ -46,3 +64,4 @@ public class RainbowBoxFunction{
         }
     }
 }
+
