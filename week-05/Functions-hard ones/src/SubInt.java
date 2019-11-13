c6c229f25f196eedc37d742eb037c37c8b5ce5e8
viktoria-sarosi <domanchana@gmail.com>
2019-11-13 22:08:18 +0100

    substring

--- week-05/Functions-hard ones/src/SubInt.java
+++ week-05/Functions-hard ones/src/SubInt.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SubInt {
     public static void main(String[] args) {
         //  Create a function that takes a number and an array of integers as a parameter
@@ -9,10 +11,27 @@ public class SubInt {
         //  should print: `[0, 1, 4]`
         //System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
         //  should print: '[]'
-        public static int subInt(int number, int[] sequence){
-            for(int element : sequence){
+        int n = 1;
+        int[] numbers = new int[] {1, 11, 34, 52, 61};
+        System.out.println(subIntFinder(n, numbers));
 
+        }
+
+    private static ArrayList subIntFinder(int n, int[] numbers) {
+        String nu = String.valueOf(n); //stringgé alakitja a számot
+        CharSequence nunu = ((CharSequence) nu); //characterré alakitja  a stringet
+        String [] newnumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            String a = String.valueOf(numbers[i]); // stringgé alakitja az arrayben lévő számokat
+            newnumbers[i] = a; //bepakolja az üres array-be
+        }
+        ArrayList<Integer> indices = new ArrayList<>();
+        for (int i = 0; i <newnumbers.length ; i++) {
+            if (newnumbers[i].contains(nunu) ){
+                indices.add(i);
             }
         }
-    }
+        return indices;
+        }
 }
+
