f95d36d36c122c01d930b9e7fd2600ef598da2d0
viktoria-sarosi <domanchana@gmail.com>
2019-11-25 22:29:56 +0100

    bunniesagain without odd-even condition

--- week-08/Recursion/src/BunniesAgain.java
+++ week-08/Recursion/src/BunniesAgain.java
@@ -1,21 +1,17 @@
 public class BunniesAgain {
     public static void main(String[] args) {
-        System.out.println(bunniecounter(19));
+        System.out.println(bunniecounter(20));
     }
 
     private static int bunniecounter(int n) {
-        if (n % 2 == 0) {
             if (n == 2) {
                 return 5;
-            } else {
-                return 5 + bunniecounter(n - 2);
             }
-        } else {
             if (n == 1) {
-                return 3;
+                return 2;
             } else {
                 return 5 + bunniecounter(n - 2);
             }
         }
     }
-}
+
