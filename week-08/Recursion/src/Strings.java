fb61b6cd794752b84c94910fddbd366a73e5536b
viktoria-sarosi <domanchana@gmail.com>
2019-11-26 10:33:06 +0100

    strings

--- week-08/Recursion/src/Strings.java
+++ week-08/Recursion/src/Strings.java
@@ -1,11 +1,21 @@
-public class String {
-    public static void main(String[] args) {
-        String text1 = "xhxdxf";
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
-        changeTheXs(text1);
+public class Strings {
+    public static void main(String[] args) { // még csak az első x-et változtatja át
+        String text1 = "xhxdxf  egfx xjhxx";
+
+        System.out.println(changeTheXs(text1));
     }
 
-    private static void changeTheXs(String text1) {
-        
+    private static String changeTheXs(String text1) {
+        if (text1.length() < 1){
+            return text1;
+        }
+        else{
+            if("x".equals(String.valueOf(text1.charAt(0)))){ // Ha "x" a szöveg első karaktere Stringgé alakitva.
+                text1 = "y" + text1.substring(1); // akkor irja felöl a szöveget y-nal és az utána jövő szövegrésszel
+            }
+            return text1.charAt(0) + changeTheXs(text1.substring(1)); // adja vissza az első betűt (ami vagy nem x, vagy x, de már átalakitva y-né) plusz a maradékot visszküldi a functionba.
+        }
     }
 }
