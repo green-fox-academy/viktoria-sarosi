01f4e2d339dc6a51b4122add034f001650f5420d
viktoria-sarosi <domanchana@gmail.com>
2020-01-05 23:12:10 +0100

    substring is found

---
+++ week-06/Data Structures Old Ones/src/SubStrList.java
@@ -0,0 +1,27 @@
+public class SubStrList {
+    public static void main(String[] args) {
+        //  Create a function that takes a string and a list of string as a parameter
+        //  Returns the index of the string in the list where the first string is part of
+        //  Only need to find the first occurence and return the index of that
+        //  Returns `-1` if the string is not part any of the strings in the list
+
+        //  Example
+        String[] searchArr = new String[]{"hello", "this", "bello", "is", "what", "I'm", "searching"};
+        System.out.println(subStrList("is", searchArr));
+        //  should print: `4`
+        System.out.println(subStrList("not", searchArr));
+        //  should print: `-1`
+    }
+
+    private static int subStrList(String stringToLookFor, String[] searchArr) {
+        int indexOfFirstOccurence = -1;
+        for (int i = 0; i < searchArr.length; i++) {
+            if (searchArr[i].contains(stringToLookFor)) {
+                indexOfFirstOccurence = i;
+                break;
+            }
+
+        }
+        return indexOfFirstOccurence;
+    }
+}
