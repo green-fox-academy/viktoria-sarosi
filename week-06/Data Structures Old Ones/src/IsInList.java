cf975015f7e2cde5f42728d44abe1f02811295ce
viktoria-sarosi <domanchana@gmail.com>
2020-01-05 09:51:44 +0100

    simplified

--- week-06/Data Structures Old Ones/src/IsInList.java
+++ week-06/Data Structures Old Ones/src/IsInList.java
@@ -2,20 +2,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IsInList {
-    public static void main(String... args){
+    public static void main(String... args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
-        checkNums(list);
+        System.out.println(checkNums(list));
         // Check if list contains all of the following elements: 4,8,12,16
         // Create a method that accepts list as an input
         // it should return "true" if it contains all, otherwise "false"
 
     }
 
-    private static void checkNums(ArrayList<Integer> list) {
-        boolean contains = false;
-        if(list.contains(4) && list.contains(8) && list.contains(12) && list.contains(16)){
-            contains = true;
-        }
-        System.out.println(contains);
+    private static boolean checkNums(ArrayList<Integer> list) {
+        boolean contains = (list.contains(4) && list.contains(8) && list.contains(12) && list.contains(16));
+        return contains;
     }
 }
