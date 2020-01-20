28373c4905c459adc2d99649f7105a783f9e58b1
viktoria-sarosi <domanchana@gmail.com>
2020-01-20 12:24:59 +0100

    sum of the odd numbers

--- week-15/src/Stream/AverageOfOddNumbers.java
+++ week-15/src/Stream/AverageOfOddNumbers.java
@@ -8,7 +8,7 @@ public class AverageOfOddNumbers {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
         OptionalDouble average = numbers.stream()
-                .filter(n -> n % 2 == 1)
+                .filter(n -> n != 0)
                 .mapToInt(n -> n)
                 .average();
         System.out.println(average);
