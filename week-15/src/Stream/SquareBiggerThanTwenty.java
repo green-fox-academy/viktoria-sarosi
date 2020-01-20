2fc7525969d0492acacc20abf7b9bbb60fd9e164
viktoria-sarosi <domanchana@gmail.com>
2020-01-20 11:55:31 +0100

    square is bigger than 20

---
+++ week-15/src/Stream/SquareBiggerThanTwenty.java
@@ -0,0 +1,14 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SquareBiggerThanTwenty {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
+        numbers.stream()
+                .map(n -> Math.pow(n, 2))
+                .filter(n -> n > 20)
+                .forEach(System.out:: println);
+    }
+}
