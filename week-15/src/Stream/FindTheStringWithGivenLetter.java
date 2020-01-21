f43cd4885256a42807dda717272ca472c9d29b46
viktoria-sarosi <domanchana@gmail.com>
2020-01-21 10:33:56 +0100

    parameter in stream

---
+++ week-15/src/Stream/FindTheStringWithGivenLetter.java
@@ -0,0 +1,15 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindTheStringWithGivenLetter {
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS");
+        String startingChar = "N";
+        cities.stream()
+                .filter(c -> c.startsWith(startingChar))
+                .forEach(c -> System.out.println(c));
+
+    }
+}
