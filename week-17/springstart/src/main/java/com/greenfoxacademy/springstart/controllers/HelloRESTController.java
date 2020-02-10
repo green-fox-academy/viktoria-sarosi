feeffb62d82f7c60e5309a7476f40083f0709f4a
viktoria-sarosi <domanchana@gmail.com>
2020-02-10 12:35:28 +0100

    (feat)greeting app

---
+++ week-17/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRESTController {
+    @RequestMapping(value = "/greeting")
+    public Greeting greeting(){
+        return new Greeting(1, "Hello World!");
+    }
+}
