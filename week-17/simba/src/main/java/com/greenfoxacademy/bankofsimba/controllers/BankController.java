f8c20c9d07deb839c4cd4af2296c0dc4180c3855
viktoria-sarosi <domanchana@gmail.com>
2020-02-14 12:32:59 +0100

    (feat)shows all the accounts

--- week-17/simba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
+++ week-17/simba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
@@ -15,11 +15,20 @@ public class BankController {
 
     public BankController() {
         accounts.add(new BankAccount("Simba", 2000, "lion"));
+        accounts.add(new BankAccount("Nala", 2000, "lion"));
+        accounts.add(new BankAccount("Timon", 1850, "meercat"));
+        accounts.add(new BankAccount("Pumba", 1900, "warthog"));
+        accounts.add(new BankAccount("Scar", 1910, "lion"));
+        accounts.add(new BankAccount("Sarabi", 1870, "lion"));
+        accounts.add(new BankAccount("Zazu", 1790, "hornbill"));
+        accounts.add(new BankAccount("Shenzi", 1800, "hyena"));
+        accounts.add(new BankAccount("Ed", 1870, "hyena"));
+
     }
 
     @GetMapping("/show")
     public String show(Model model) {
-        model.addAttribute("accounts", accounts);
+        model.addAttribute("accounts", accounts.get(0));
         return "index";
     }
 
@@ -27,5 +36,11 @@ public class BankController {
     public String htmlception(Model model){
         model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
         return "ception";
+    }
+
+    @GetMapping("/showAllAccounts")
+    public String ShowAllAccounts(Model model){
+        model.addAttribute("accounts", accounts);
+        return "showAllAccounts";
     }
 }
