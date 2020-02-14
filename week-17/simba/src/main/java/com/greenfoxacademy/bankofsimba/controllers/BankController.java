e4e5c7d50fc825c54579cb8b6ff625efd4751525
viktoria-sarosi <domanchana@gmail.com>
2020-02-14 11:46:26 +0100

    feat(fields and first account displayed)

---
+++ week-17/simba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
@@ -0,0 +1,26 @@
+package com.greenfoxacademy.bankofsimba.controllers;
+
+import com.greenfoxacademy.bankofsimba.models.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class BankController {
+
+    private List<BankAccount> accounts = new ArrayList<>();
+
+    public BankController() {
+        accounts.add(new BankAccount("Simba", 2000, "lion"));
+    }
+
+    @GetMapping("/show")
+    public String show(Model model) {
+        model.addAttribute("accounts", accounts);
+        return "index";
+    }
+
+}
