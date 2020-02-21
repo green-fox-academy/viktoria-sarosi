b6c7fc777988915b08b21602c0233b0cfcdb19dd
viktoria-sarosi <domanchana@gmail.com>
2020-02-21 13:09:01 +0100

    feat(add new student)

--- week-18/src/main/java/com/greenfoxacademy/dependency/gfa/GfaController.java
+++ week-18/src/main/java/com/greenfoxacademy/dependency/gfa/GfaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GfaController {
@@ -21,8 +22,20 @@ public class GfaController {
     }
 
     @GetMapping("/gfa/list")
-    public String renderList(Model model){
+    public String renderList(Model model) {
         model.addAttribute("list", studentService.findAll());
         return "gfa/list";
+    }
+
+    @PostMapping("gfa/add")
+    public String addNewStudent(String name) {
+        studentService.save(name);
+        return "gfa/add";
+    }
+
+    @GetMapping("gfa/add")
+    public String renderAdd(Model model) {
+        model.addAttribute("name", "name");
+        return "gfa/add";
     }
 }
