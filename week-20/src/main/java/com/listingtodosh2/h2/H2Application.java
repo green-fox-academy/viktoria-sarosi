375721a53690205dba8bffeca46cc55c83a8978a
viktoria-sarosi <domanchana@gmail.com>
2020-03-02 18:02:54 +0100

    listing todos h2

--- week-20/src/main/java/com/listingtodosh2/h2/H2Application.java
+++ week-20/src/main/java/com/listingtodosh2/h2/H2Application.java
@@ -1,13 +1,32 @@
 package com.listingtodosh2.h2;
 
+import com.listingtodosh2.h2.models.Todo;
+import com.listingtodosh2.h2.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class H2Application {
+public class H2Application implements CommandLineRunner {
+
+    private TodoRepository todoRepository;
+
+    @Autowired
+    public H2Application(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(H2Application.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Todo todo = new Todo("Learn ORM");
+        todoRepository.save(todo);
+        Todo otherTodo = new Todo("Finish excercise");
+        todoRepository.save(otherTodo);
+    }
 }
--- week-20/src/main/java/com/listingtodosh2/h2/models/Todo.java
+++ week-20/src/main/java/com/listingtodosh2/h2/models/Todo.java
@@ -1,18 +1,40 @@
 package com.listingtodosh2.h2.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "todos")
 public class Todo {
+
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private boolean urgent;
     private boolean done;
 
     public Todo() {
+    }
+
+    public Todo(String title) {
+        this.title = title;
         urgent = false;
         done = false;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
